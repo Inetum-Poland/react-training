39da88866672c390e0ff6a01e06766aa1b0413a8
Paweł Adamowicz <p.adamowicz@ideaigniter.pl>
2026-02-12 20:36:29 +0100

    feat: lesson 4 details

--- frontend/src/LessonDetails.tsx
+++ frontend/src/LessonDetails.tsx
@@ -1,10 +1,10 @@
-import { GoalIcon, InfoIcon } from "lucide-react";
+import { GoalIcon } from "lucide-react";
 
 export default function LessonDetails() {
   return (
     <>
       <h1 className="text-2xl font-bold text-neutral-800">
-        Lekcja 3 – Deep dive
+        Lekcja 4 – Formularze
       </h1>
 
       <h2 className="text-xl font-semibold my-6 flex gap-4 items-center text-neutral-800">
@@ -13,9 +13,7 @@ export default function LessonDetails() {
       </h2>
 
       <ul className="list-disc pl-6">
-        <li>Jak używać react context</li>
-        <li>Jak używać tanstack query</li>
-        <li>Jak zrobić walidację formularza</li>
+        <li>Jak używać tanstack @tanstack/react-form</li>
       </ul>
 
       <h2 className="text-xl font-semibold my-6 flex gap-4 items-center text-neutral-800">
@@ -23,26 +21,71 @@ export default function LessonDetails() {
         <span>Zadania do wykonania</span>
       </h2>
 
-      <p className="bg-blue-200 p-4 rounded-md mb-6">
-        <InfoIcon size={16} className="inline mb-1 mr-2" />
-        Zadania będziemy realizować krok po kroku podczas livecodingu. Zachęcam
-        do równoległego odtwarzania wszystkiego u siebie lokalnie. Kod, który
-        widzisz, to stan finalny lekcji 1 – możesz usunąć niepotrzebne elementy,
-        takie jak <strong>Refresh</strong> i odświeżanie krypto co{" "}
-        <strong>10 sekund</strong>.
-      </p>
-
       <ul className="list-decimal pl-6 space-y-4">
         <li>
-          W pliku <strong>StockItem.tsx</strong> dodaj metodę do walidacji formularza, może się nazywać <strong>isSubmitDisabled</strong>. Zadaniem tej metody, jest sprawdzenie, czy podana wartość jest numeryczna, większa od zera, oraz czy mamy środki na koncie. Jeśli chcemy kupić coś za kwotę np. 100 000 zł a mamy balans 99 999 zł, to nie powinniśmy mieć takiej możliwości. Metoda powinna sprawdzać te warunki i w zależności od tego ustawiać <strong>disabled</strong> na przycisku formularza.
+          Utwórz nowy plik o nazwie Form.tsx i dodaj go do menu po lewej
+          stronie. Utworzymy tam formularz.
         </li>
-         <li>
-          Nad przyciskiem dodaj informację <strong>Nie posiadasz wystarczająco dużo środków</strong> w przypadku, kiedy wybierzesz za dużą ilość kryptowaluty. Utwórz metodę pomocniczą <strong>isCostWarning</strong> a do pobrania stanu konta użyj <strong>useBalance</strong>.
+        <li>
+          Utwórz nowy <strong>obiekt form</strong> według wytycznych z{" "}
+          <a
+            className="text-green-700 underline"
+            href="https://tanstack.com/form/latest/docs/overview">
+            dokumentacji
+          </a>
+          .
         </li>
-        <li>Dodaj czerwony border jeśli użytkownik kliknie i nic nie wpisze, oraz jeśli nie posiadasz wystarczająco dużo środków. Do tego celu użyj <strong>onBlur</strong> i dodaj nowy <strong>useState</strong> który będzie przechowywał stan błędu dla inputa.</li>
-        <li>Wydziel <strong>useCryptoList</strong> do oddzielnego context.</li>
-        <li>Wydziel <strong>useUserCrypto</strong> do oddzielnego context.</li>
-        <li>Dodaj tanstack query do obsługi API</li>
+        <li>
+          Wydziel komponenty do plików. To co będzie w <strong>children</strong>{" "}
+          wydziel jako oddzielny parametryzowany komponent.
+        </li>
+        <li>
+          Wydziel walidatory z <strong>validators</strong> do oddzielnych metod.
+        </li>
+        <li>
+          Zamień teraz walidator na <strong>ZOD</strong>. Użyj linku z{" "}
+          <a
+            className="text-green-700 underline"
+            href="https://tanstack.com/form/latest/docs/framework/react/guides/validation#validation-through-schema-libraries">
+            dokumentacji
+          </a>
+        </li>
+        <li>
+          Spraw aby button <strong>Submit</strong> był nieaktywny, gdy formularz
+          jest nieprawidłowy. Użyj linku z{" "}
+          <a
+            className="text-green-700 underline"
+            href="https://tanstack.com/form/latest/docs/framework/react/guides/validation#preventing-invalid-forms-from-being-submitted">
+            dokumentacji
+          </a>
+        </li>
+        <li>
+          Dodaj pole typu checkbox oraz select. Dla select użyj np.{" "}
+          <code className="px-1 py-0.5 rounded bg-neutral-100 text-neutral-800">
+            role: z.enum(["junior", "mid", "senior"])
+          </code>
+          . Do checkboxów użyj linku z{" "}
+          <a
+            className="text-green-700 underline"
+            href="https://tanstack.com/form/latest/docs/framework/react/guides/basic-concepts#field">
+            dokumentacji
+          </a>
+          .
+        </li>
+        <li>
+          Zrób teraz tak, że jeśli checkbox będzie zaznaczony, to pojawi się
+          dodatkowe pole tekstowe do wypełnienia np. comment.
+        </li>
+        <li>
+          Jak wybiorę opcję "senior" w select, nie muszę wpisywać imienia.
+          Możesz korzystać z{" "}
+          <a
+            className="text-green-700 underline"
+            href="https://zod.dev/api?id=superrefine">
+            dokumentacji ZOD
+          </a>
+          .
+        </li>
       </ul>
     </>
   );
